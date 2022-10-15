c3bc2268b33679c9d4ed7831c59a9bfedbc8be5a
Matt Huntsberry <matt.huntsberry@nielsen.com>
2022-10-15 12:33:51 -0500

    update port

--- keystone.ts
+++ keystone.ts
@@ -1,22 +1,22 @@
-import "dotenv/config";
-import { config, createSchema } from "@keystone-next/keystone/schema";
-import { createAuth } from "@keystone-next/auth";
+import 'dotenv/config';
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import { createAuth } from '@keystone-next/auth';
 import {
   withItemData,
   statelessSessions,
-} from "@keystone-next/keystone/session";
-import { User } from "./schemas/User";
-import { Product } from "./schemas/Product";
-import { ProductImage } from "./schemas/ProductImage";
-import { CartItem } from "./schemas/CartItem";
-import { OrderItem } from "./schemas/OrderItem";
-import { Order } from "./schemas/Order";
-import { Role } from "./schemas/Role";
+} from '@keystone-next/keystone/session';
+import { User } from './schemas/User';
+import { Product } from './schemas/Product';
+import { ProductImage } from './schemas/ProductImage';
+import { CartItem } from './schemas/CartItem';
+import { OrderItem } from './schemas/OrderItem';
+import { Order } from './schemas/Order';
+import { Role } from './schemas/Role';
 
-import { insertSeedData } from "./seed-data";
-import { sendPasswordResetEmail } from "./lib/mail";
-import { extendGraphQlSchema } from "./mutations";
-import { permissionsList } from "./schemas/fields";
+import { insertSeedData } from './seed-data';
+import { sendPasswordResetEmail } from './lib/mail';
+import { extendGraphQlSchema } from './mutations';
+import { permissionsList } from './schemas/fields';
 
 const databaseURL = process.env.DATABASE_URL;
 
@@ -26,11 +26,11 @@ const sessionConfig = {
 };
 
 const { withAuth } = createAuth({
-  listKey: "User",
-  identityField: "email",
-  secretField: "password",
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
   initFirstItem: {
-    fields: ["name", "email", "password"],
+    fields: ['name', 'email', 'password'],
     // Todo: Add in initial roles here
   },
   passwordResetLink: {
@@ -47,13 +47,14 @@ export default withAuth(
         origin: [process.env.FRONTEND_URL],
         credentials: true,
       },
+      port: 8080,
     },
     db: {
-      adapter: "mongoose",
+      adapter: 'mongoose',
       url: databaseURL,
       // TODO: Add data seeding here
       async onConnect(keystone) {
-        if (process.argv.includes("--seed-data"))
+        if (process.argv.includes('--seed-data'))
           await insertSeedData(keystone);
       },
     },
@@ -74,7 +75,7 @@ export default withAuth(
     },
     // Todo: Add session values here
     session: withItemData(statelessSessions(sessionConfig), {
-      User: `id name email role { ${permissionsList.join(" ")} }`,
+      User: `id name email role { ${permissionsList.join(' ')} }`,
     }),
   })
 );
